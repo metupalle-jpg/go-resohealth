2c77bd793c80c3a9ea7483d08defc3ca8f9d96f1
metupalle <metupalle@gmail.com>
2026-04-03 11:26:47 +0000

    feat: Add Dockerfile and Cloud Build config for Cloud Run deployment

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  allowedDevOrigins: ['https://3000-w-metupalle-mniisa2i.cluster-g6x7qqwepzfpww4wv6o4hwxsgc.cloudworkstations.dev'],
+}
+module.exports = nextConfig
